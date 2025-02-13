04fe55439f51f66114a61643e5c82afd4cbb8b92
Jacek Gizinski <jacektrg+github@gmail.com>
2025-02-13 20:20:12 +0100

    Checking for env vars.
    Listening for 'smss' command, destructuring provided url and optional commentsDepth.
    Validating url, calling deferResponse with necessary data to request a screenshot.
    Sending initial response withing 3seconds (discord limit) to let client render the request was accepted.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
 import 'dotenv/config';
 import express from 'express';
 import {
+	InteractionResponseFlags,
 	InteractionResponseType,
 	InteractionType,
 	verifyKeyMiddleware,
 } from 'discord-interactions';
+import { isURL } from 'class-validator';
+import { deferResponse } from './utils';
+
+export const {
+	APP_ID,
+	PUBLIC_KEY,
+	DISCORD_TOKEN,
+	NODE_ENV,
+	SOCIAL_MEDIA_SCREENSHOT_API_URI,
+	SOCIAL_MEDIA_SCREENSHOT_API_URI_DEV,
+} = process.env;
+export const API_URI =
+	NODE_ENV === 'production'
+		? SOCIAL_MEDIA_SCREENSHOT_API_URI
+		: SOCIAL_MEDIA_SCREENSHOT_API_URI_DEV;
+
+if (!APP_ID || !PUBLIC_KEY || !DISCORD_TOKEN || !API_URI) {
+	throw new Error('Define ENVS');
+}
 
 // Create an express app
 const app = express();
@@ -17,20 +37,62 @@ const PORT = process.env.PORT || 3000;
  */
 app.post(
 	'/interactions',
-	verifyKeyMiddleware(process.env.PUBLIC_KEY),
+	verifyKeyMiddleware(PUBLIC_KEY),
 	async function (req, res) {
 		// Interaction id, type and data
-		const { type, data } = req.body;
+		const { type, data, token, member } = req.body;
 
 		/**
 		 * Handle verification requests
 		 */
 		if (type === InteractionType.PING) {
-			return res.send({ type: InteractionResponseType.PONG });
+			res.send({ type: InteractionResponseType.PONG });
+			return;
+		}
+
+		/**
+		 * Handle slash command requests
+		 * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+		 */
+		if (type === InteractionType.APPLICATION_COMMAND) {
+			const { name, options } = data;
+
+			// Take screenshot command
+			if (name === 'smss') {
+				// Send a message into the channel where command was triggered from
+				const url = options[0];
+				const commentsDepth = options[1];
+				if (!url) {
+					res.status(400).json({ error: 'url is required' });
+					return;
+				}
+				if (
+					!isURL(url.value, {
+						protocols: ['http', 'https'],
+						require_protocol: true,
+					})
+				) {
+					res.status(400).json({ error: 'url is not valid' });
+					return;
+				}
+				deferResponse(token, member.user.id, url.value, commentsDepth?.value);
+				res.send({
+					type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
+					data: {
+						flags: InteractionResponseFlags.EPHEMERAL,
+					},
+				});
+				return;
+			}
+
+			console.error(`unknown command: ${name}`);
+			res.status(400).json({ error: 'unknown command' });
+			return;
 		}
 
 		console.error('unknown interaction type', type);
-		return res.status(400).json({ error: 'unknown interaction type' });
+		res.status(400).json({ error: 'unknown interaction type' });
+		return;
 	}
 );
 
