88abb2e4eceebab041059ebc12c579932900817d
Jacek Gizinski <jacektrg+github@gmail.com>
2025-02-26 18:39:14 +0100

    Updating command name.
    Updating descriptions and max value for cd option.

--- src/commands.ts
+++ src/commands.ts
@@ -2,22 +2,22 @@ import 'dotenv/config';
 import { InstallGlobalCommands } from './utils';
 
 const SCREENSHOT_COMMAND = {
-	name: 'smss',
+	name: 'ssit',
 	description: 'Take screenshot of social media post',
 	options: [
 		{
 			type: 3,
 			name: 'url',
-			description: 'Post link',
+			description: 'Post link.',
 			required: true,
 		},
 		{
 			type: 4,
 			name: 'cd',
-			description: 'How many comments to include',
+			description: 'How many comments to include (not supported everywhere).',
 			required: false,
 			min_value: 0,
-			max_value: 10,
+			max_value: 20,
 		},
 	],
 	type: 1,
