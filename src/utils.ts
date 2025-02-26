3071523a69e48c5e3b8017707191caa3b4880a12
Jacek Gizinski <jacektrg+github@gmail.com>
2025-02-26 18:50:54 +0100

    Adding requestScreenshot function which sends request to api required with url and discordId data. Returns screenshot url.
    Adding sendFollowupResponse which returns deferred message screenshot link back to discord client and edits original message.
    Adding sendDMToUser which sends Direct Message to user with screenshot link.
    Adding createDMChannelWithUser used to create DM with discord user.
    Updating deferResponse to used functions mentioned above to return screernshot url back to discord user or folloup error response in catch block.

--- src/utils.ts
+++ src/utils.ts
@@ -1,26 +1,22 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import 'dotenv/config';
-import { APP_ID, DISCORD_TOKEN } from './app';
+import { API_URI, APP_ID, DISCORD_TOKEN } from './app';
+import { User } from './types';
 
 export async function DiscordRequest(
 	endpoint: string,
 	options: { method: string; data?: any }
 ) {
-	try {
-		// append endpoint to root API URL
-		const url = 'https://discord.com/api/v10/' + endpoint;
-		const res = await axios(url, {
-			headers: {
-				Authorization: `Bot ${DISCORD_TOKEN}`,
-				'Content-Type': 'application/json; charset=UTF-8',
-			},
-			...options,
-		});
-		return res;
-	} catch (error) {
-		console.log(error);
-		throw error;
-	}
+	// append endpoint to root API URL
+	const url = 'https://discord.com/api/v10/' + endpoint;
+	const res = await axios(url, {
+		headers: {
+			Authorization: `Bot ${DISCORD_TOKEN}`,
+			'Content-Type': 'application/json; charset=UTF-8',
+		},
+		...options,
+	});
+	return res;
 }
 
 export async function InstallGlobalCommands(commands: any) {
@@ -32,23 +28,81 @@ export async function InstallGlobalCommands(commands: any) {
 			method: 'PUT',
 			data: commands,
 		});
-	} catch (err) {
-		console.error(err);
+	} catch (error: unknown) {
+		console.error(error);
 	}
 }
 
 export async function deferResponse(
 	token: string,
-	userId: string,
+	user: User,
 	url: string,
 	commentsDepth?: number
 ): Promise<void> {
 	try {
-		// request screenshot from API
-		// send folloup response
-		// send dm to user?
-	} catch (err) {
-		console.error(err);
-		throw err;
+		const screenshotUrl = await requestScreenshot(url, user.id, commentsDepth);
+		sendFollowupResponse(token, screenshotUrl);
+		sendDMToUser(user.id, screenshotUrl);
+	} catch (error: unknown) {
+		if (error instanceof AxiosError) {
+			console.error(error.response);
+			sendFollowupResponse(
+				token,
+				error.response?.data.message || 'Error taking a screenshot.'
+			);
+		} else {
+			console.error(error);
+			sendFollowupResponse(token, 'Error taking a screenshot.');
+		}
 	}
+}
+
+export async function requestScreenshot(
+	url: string,
+	userId: string,
+	commentsDepth?: number
+): Promise<string> {
+	const hostname = new URL(url).hostname.replace('www.', '');
+	let service = hostname.slice(0, hostname.lastIndexOf('.'));
+	if (service === 'x') {
+		service = 'twitter';
+	}
+	const resAPI: AxiosResponse<string> = await axios.post(
+		`${API_URI}/screenshot/${service}`,
+		{ postUrl: url, commentsDepth: commentsDepth || 0, discordId: userId },
+		{ headers: { 'Content-Type': 'application/json' } }
+	);
+	return resAPI.data;
+}
+
+export async function sendFollowupResponse(
+	token: string,
+	message: string
+): Promise<void> {
+	const endpoint = `webhooks/${APP_ID}/${token}/messages/@original`;
+	await DiscordRequest(endpoint, {
+		method: 'PATCH',
+		data: { content: message },
+	});
+}
+
+export async function sendDMToUser(
+	userId: string,
+	message: string
+): Promise<void> {
+	const channelId = await createDMChannelWithUser(userId);
+	const endpoint = `channels/${channelId}/messages`;
+	await DiscordRequest(endpoint, {
+		method: 'POST',
+		data: { content: message },
+	});
+}
+
+export async function createDMChannelWithUser(userId: string): Promise<string> {
+	const endpoint = `users/@me/channels`;
+	const res = await DiscordRequest(endpoint, {
+		method: 'POST',
+		data: { recipient_id: userId },
+	});
+	return res.data.id;
 }
