9231785231f72c98529e7ad3fc11da745dfa02de
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-11 20:00:25 +0100

    Updating descriptions.
    Adding optional nitter field.

--- src/commands.ts
+++ src/commands.ts
@@ -8,16 +8,24 @@ const SCREENSHOT_COMMAND = {
 		{
 			type: 3,
 			name: 'url',
-			description: 'Post link.',
+			description: 'Post link to screenshot',
 			required: true,
 		},
 		{
 			type: 4,
 			name: 'cd',
-			description: 'How many comments to include (not supported everywhere).',
+			description:
+				'How many comments to include? (not supported by FB videos, IG and Twitter)',
 			required: false,
 			min_value: 0,
-			max_value: 20,
+			max_value: 10,
+		},
+		{
+			type: 5,
+			name: 'nitter',
+			description:
+				'Use nitter as alternative to include comments? (for Twitter links only)',
+			required: false,
 		},
 	],
 	type: 1,
